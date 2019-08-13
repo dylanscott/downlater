d45773fe8eadf33e56670e066073489ab1f4f1d4
Dylan Scott <dylan.scott@gmail.com>
2019-08-12 20:44:18 -0700

    code for adding and reading from download queue

---
+++ src/db/index.ts
@@ -0,0 +1,36 @@
+import * as path from 'path';
+import { Database } from 'sqlite3';
+import { promisify } from 'util';
+
+import { VideoInfo } from '../ytdl';
+
+const DB = new Database(path.resolve(__dirname, '../../db/db.bin'));
+
+export interface QueuedDownload {
+    id: number;
+    info: VideoInfo;
+    format: string;
+}
+
+export interface YTDLQueueRow {
+    id: number;
+    json: string;
+    format: string;
+}
+
+export async function fetchDownloadQueue(): Promise<QueuedDownload[]> {
+    const rows = await promisify((cb: (err: Error | null, rows: YTDLQueueRow[]) => void) => {
+        return DB.all('SELECT * FROM YTDLQueue;', cb);
+    })();
+    return rows.map((row) => ({
+        id: row.id,
+        format: row.format,
+        info: JSON.parse(row.json),
+    }));
+}
+
+export async function appendToDownloadQueue(info: VideoInfo, format: string): Promise<void> {
+    await promisify((cb: (err: Error | null) => void) => {
+        return DB.run('INSERT INTO YTDLQueue(json, format) VALUES (?, ?)', JSON.stringify(info), format, cb);
+    })();
+}
