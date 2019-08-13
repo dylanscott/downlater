b14365b27ce11ffe7ee6d6bf7175353e49553461
Dylan Scott <dylan.scott@gmail.com>
2019-08-12 21:04:38 -0700

    check the time in dldrain

--- src/dldrain.ts
+++ src/dldrain.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import moment from 'moment';
 
 import { downloadVideo } from './ytdl';
 import { fetchDownloadQueue, markComplete } from './db';
@@ -7,11 +8,19 @@ async function drain() {
     const queue = await fetchDownloadQueue();
     for (const entry of queue) {
         try {
+            checkTime();
             await downloadVideo(entry.info, entry.format);
             await markComplete(entry.id);
         } catch (err) {
             console.error(`failed to download ${entry.info.display_id}`, err);
         }
+    }
+}
+
+function checkTime() {
+    const time = moment();
+    if (time.hour() < 2 || time.hour() >= 8) {
+        throw new Error("The time isn't right.");
     }
 }
 
