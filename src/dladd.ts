0a73bf58c55d69b968e6082efdd0c060649e1b14
Dylan Scott <dylan.scott@gmail.com>
2019-08-17 23:39:51 -0700

    factor out allowed format

--- src/dladd.ts
+++ src/dladd.ts
@@ -3,16 +3,18 @@
 import { fetchVideoInfo } from './ytdl';
 import { appendToDownloadQueue } from './db';
 
+const ALLOWED_FORMAT_ID = '22';
+
 async function add(url: string) {
     const info = await fetchVideoInfo(url);
 
     // currently only supporting format = 22
-    const matchingFormat = info.formats.find((format) => format.format_id === '22');
+    const matchingFormat = info.formats.find((format) => format.format_id === ALLOWED_FORMAT_ID);
     if (!matchingFormat) {
-        throw new Error(`video ${url} does not have format 22`);
+        throw new Error(`video ${url} does not have format ${ALLOWED_FORMAT_ID}`);
     }
 
-    await appendToDownloadQueue(info, '22');
+    await appendToDownloadQueue(info, ALLOWED_FORMAT_ID);
 }
 
 if (process.argv[2]) {
